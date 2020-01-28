9da9864b4cb1d8fe1049c57c88a0bd12a132090c
naveenvunyala <naveenvunyala9@gmail.com>
2020-01-28 11:40:11 +0530

    my first commit

---
+++ sum.java
@@ -0,0 +1,12 @@
+  class sum
+{   
+   public static void main(String[] args){
+
+       int a,b,c;
+       a = 50;
+       b = 30;
+       c  = 20;
+
+       System.out.println(The sum of three no+"c");
+   }
+}
